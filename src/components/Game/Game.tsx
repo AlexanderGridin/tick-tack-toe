9ee032d5daa1268da9054db8232926d1f288d7d7
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-08-20 15:30:26 +0300

    Add simple history

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -5,30 +5,30 @@ import { BoardCellValue } from "../GameCell";
 import { calculateNexMove, calculateWinner, createCells, getFirstMove } from "../../utils";
 
 export const Game = () => {
-  const [cells, setCells] = useState<BoardCellValue[]>(createCells());
   const [move, setMove] = useState(getFirstMove());
   const [winner, setWinner] = useState<BoardCellValue>(null);
+  const [history, setHistory] = useState<Array<BoardCellValue[]>>([createCells()]);
 
   const handleCellClick = (index: number) => {
+    const cells = history[history.length - 1].slice();
     if (cells[index] || winner) return;
 
-    const cellsCopy = cells.slice();
-    cellsCopy[index] = move;
+    cells[index] = move;
+    setHistory([...history, cells]);
 
-    setCells(cellsCopy);
+    const calculatedWinner = calculateWinner(cells);
+    if (calculatedWinner) {
+      setWinner(calculatedWinner);
+      setMove(null);
 
-    const calculatedWinner = calculateWinner(cellsCopy);
-    if (!calculatedWinner) {
-      setMove(calculateNexMove(move));
       return;
     }
 
-    setWinner(calculatedWinner);
-    setMove(null);
+    setMove(calculateNexMove(move));
   };
 
   const handleRestartClick = () => {
-    setCells(createCells());
+    setHistory([createCells()]);
     setMove(getFirstMove());
     setWinner(null);
   };
@@ -43,7 +43,49 @@ export const Game = () => {
             <button onClick={handleRestartClick}>Restart</button>
           </>
         )}
-        <GameBoard cells={cells} onCellClick={handleCellClick} />
+
+        <div className={style.boardContainer}>
+          <GameBoard cells={history[history.length - 1]} onCellClick={handleCellClick} />
+
+          <div style={{ marginLeft: "25px" }}>
+            <h2>History:</h2>
+
+            <ol style={{ margin: 0, padding: 0, listStylePosition: "inside" }}>
+              {history.map((_, index) => {
+                const isFirst = index === 0;
+                const isLast = index === history.length - 1;
+
+                if (isFirst) {
+                  return (
+                    <li
+                      key={index}
+                      style={{
+                        backgroundColor: "#B48EAD",
+                        padding: "5px",
+                        color: "#FFF",
+                      }}
+                    >
+                      Start of the game
+                    </li>
+                  );
+                }
+
+                return (
+                  <li
+                    key={index}
+                    style={{
+                      backgroundColor: isLast ? "#5E81AC" : isFirst ? "#B48EAD" : "#FFF",
+                      padding: "5px",
+                      color: isLast || isFirst ? "#FFF" : "#000",
+                    }}
+                  >
+                    {isLast && winner ? `Move #${index} (winner)` : `Move #${index}`}
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
+        </div>
       </div>
     </div>
   );
